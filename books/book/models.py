cfab8950c2ac8e2194f63ff77e6a77113929b90c
jltavera <jl.tavera@uniandes.edu.co>
2022-10-02 21:22:10 -0500

    Serializer

---
+++ books/book/models.py
@@ -0,0 +1,8 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length = 50)
+    description = models.TextField()
+
---
+++ books/book/urls.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+
+from .viewsets import BookViewSet
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
