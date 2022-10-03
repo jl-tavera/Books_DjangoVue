a7e4e9fb9237adf88d3343320724ebcce89a81e3
jltavera <jl.tavera@uniandes.edu.co>
2022-10-03 15:20:52 -0500

    Danniel me pauso

--- books/frontend/src/components/Book/ListBook.vue
+++ books/frontend/src/components/Book/ListBook.vue
@@ -1,21 +1,35 @@
-<template lang="html"> 
- <div class="container">
-    <div class="row">
-        <div class="col text-left">
-            <h2>LIstado de Libros</h2>
+<template lang="html">
+    <div class="container">
+        <div class="row">
+            <div class="col text-left">
+                <h2>Listado de Libros</h2>
+                <div class="col-md-12">
+                    <b-table striped hover :items="books" :fields="fields">
+                    </b-table>
+                </div>
+            </div>
         </div>
     </div>
- </div>
-
-
+    
 </template>
+
 <script>
+    export default {
+        data () {
+            return {
+                fields: [
+                    {key:'title', label:'Título'},
+                    {key:'description', label:'Descripción'},
+                    {key:'action', label:'Acción'}
+                ],
+                books: []
+            }
+        }
 
-export default {
+    }
 
-}
 </script>
 
-<style scoped>
+<style lang="css" scoped>
 
 </style>
--- books/frontend/src/main.js
+++ books/frontend/src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vue from 'vue'
-import { BootstrapVue } from 'bootstrap-vue'
-import 'node_modules/bootstrap/scss/bootstrap.scss';
-import 'node_modules/bootstrap-vue/src/index.scss';
-import './app.scss'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
